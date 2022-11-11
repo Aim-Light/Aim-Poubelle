3dc889994ead62073797cd2bb8d94584cdf86a25
7M Therapyst <7m@lockz.fr>
2022-11-11 12:32:18 +0400

    Create fr.lua

---
+++ Aim-Poubelle/locales/fr.lua
@@ -0,0 +1,20 @@
+-- FR = Français
+Translations = {
+    ["FR"] = {
+        error = {
+            cooldown = 'Tu dois attendre %s secondes pour rechercher à nouveau',
+            notLegit = "Tu dois patienter avant de pouvoir fouiller !",
+            hasBeenSearched = "Cette poubelle à déjà été fouillé!",
+            nothingFound = "Tu n'as rien trouvé!",
+        },
+
+        progressbar = {
+            searching = "Tu fouilles la poubelle..",
+        },
+        
+        gains = {
+            money = "Tu as trouvé [~b~%s$~s~]",
+            found = "Tu as trouvé [~b~%s~s~] !",
+        }
+    }
+}
